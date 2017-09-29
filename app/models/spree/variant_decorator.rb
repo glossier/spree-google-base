e7fc26c5d6873bdf9807d10d53266bf492da685d
Braden <me@braidn.com>
2017-09-29 11:27:26 -0400

    Lint Variant Decorator.
    Fix some rubocop issues that are flagged on this file. Mainly the use of
    self and double quotes when not interpolating strings.

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,10 +1,10 @@
 module Spree
-  Variant.class_eval do
+  module VariantDecorator
     def google_base_description
       # description is limited to 5000 characters
       # see https://support.google.com/merchants/answer/188494 for details
-      #description[0...5000]
-      self.product.content(:excerpt)
+      # description[0...5000]
+      product.content(:excerpt)
     end
 
     def google_base_condition
@@ -13,9 +13,9 @@ module Spree
 
     def google_base_availability
       if warehouse_count_on_hand > 0
-        "in stock"
+        'in stock'
       else
-        "out of stock"
+        'out of stock'
       end
     end
 
@@ -24,23 +24,23 @@ module Spree
     end
 
     def google_base_gtin
-      self.gtin
+      gtin
     end
 
     def google_base_id
-      self.sku
+      sku
     end
 
     def google_base_weight
-      self.weight
+      weight
     end
 
     def google_base_name
-      self.product.name
+      product.name
     end
 
     def google_base_price
-      self.product.price
+      product.price
     end
 
     def google_base_image_size
@@ -60,14 +60,15 @@ module Spree
     end
 
     private
+
     def warehouse_count_on_hand
-      warehouse_id = stock_locations.find_by(name: "Warehouse").id
+      warehouse_id = stock_locations.find_by(name: 'Warehouse').id
       for stock_item in stock_items
         if stock_item.stock_location_id == warehouse_id
           return stock_item.count_on_hand
         end
       end
     end
-
   end
 end
+::Spree::Variant.prepend ::Spree::VariantDecorator
